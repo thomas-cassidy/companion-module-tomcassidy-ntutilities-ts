cb0f72d42ef4b14b359d2a3e7a1f0e2e69678646
thomas-cassidy <tomcass@me.com>
2025-09-26 15:19:53 +0100

    initial commit

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,33 @@
 import { Regex, type SomeCompanionConfigField } from '@companion-module/base'
 
 export interface ModuleConfig {
-	host: string
-	port: number
+	target: string
+	send_port: number
+	rec_port: number
 }
 
 export function GetConfigFields(): SomeCompanionConfigField[] {
 	return [
 		{
 			type: 'textinput',
-			id: 'host',
+			id: 'target',
 			label: 'Target IP',
 			width: 8,
 			regex: Regex.IP,
 		},
 		{
 			type: 'number',
-			id: 'port',
-			label: 'Target Port',
+			id: 'send_port',
+			label: 'Send Port',
+			width: 4,
+			min: 1,
+			max: 65535,
+			default: 8000,
+		},
+		{
+			type: 'number',
+			id: 'rec_port',
+			label: 'Receive Port',
 			width: 4,
 			min: 1,
 			max: 65535,
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
-import { InstanceBase, runEntrypoint, InstanceStatus, SomeCompanionConfigField } from '@companion-module/base'
+import {
+	InstanceBase,
+	runEntrypoint,
+	InstanceStatus,
+	SomeCompanionConfigField,
+	// CompanionVariableValues,
+} from '@companion-module/base'
 import { GetConfigFields, type ModuleConfig } from './config.js'
 import { UpdateVariableDefinitions } from './variables.js'
 import { UpgradeScripts } from './upgrades.js'
 import { UpdateActions } from './actions.js'
 import { UpdateFeedbacks } from './feedbacks.js'
+import { Client, Server } from 'node-osc'
 
 export class ModuleInstance extends InstanceBase<ModuleConfig> {
 	config!: ModuleConfig // Setup in init()
+	// private pollInterval?: NodeJS.Timeout
+	// private websocket?: WebSocket
+	private oscServer?: Server
+	private consoleClient?: Client
 
 	constructor(internal: unknown) {
 		super(internal)
@@ -14,16 +25,22 @@ export class ModuleInstance extends InstanceBase<ModuleConfig> {
 
 	async init(config: ModuleConfig): Promise<void> {
 		this.config = config
-
-		this.updateStatus(InstanceStatus.Ok)
+		this.updateStatus(InstanceStatus.Connecting)
 
 		this.updateActions() // export actions
 		this.updateFeedbacks() // export feedbacks
 		this.updateVariableDefinitions() // export variable definitions
+
+		// this.connectToWebSocket()
+		this.createOSCServer()
+		this.createClient()
+		this.connectToConsole()
 	}
 	// When module gets deleted
 	async destroy(): Promise<void> {
 		this.log('debug', 'destroy')
+		// this.websocket?.close()
+		this.oscServer?.close()
 	}
 
 	async configUpdated(config: ModuleConfig): Promise<void> {
@@ -45,6 +62,63 @@ export class ModuleInstance extends InstanceBase<ModuleConfig> {
 
 	updateVariableDefinitions(): void {
 		UpdateVariableDefinitions(this)
+	}
+
+	// private connectToWebSocket() {
+	// 	try {
+	// 		if (this.websocket?.OPEN) this.websocket.close()
+	// 		this.websocket = new WebSocket(`ws://${this.config.target}:${this.config.send_port}/qlab`)
+	// 		this.websocket.onopen = () => {
+	// 			this.log('debug', 'websocket connected')
+	// 		}
+	// 		this.websocket.onmessage = (event) => {
+	// 			this.log('debug', `got message: ${event.data}`)
+	// 			const payload = JSON.parse(event.data)
+	// 			this.log('debug', payload)
+
+	// 			if (payload.cg) {
+	// 				this.log('debug', payload.qlab)
+	// 				this.setVariableValues({ [`cg${payload.cg}`]: payload.value })
+	// 			}
+	// 		}
+	// 		this.websocket.onerror = (err) => {
+	// 			this.log('error', `websocket error: ${err.error}`)
+	// 		}
+	// 		thigit s.websocket.onclose = () => {
+	// 			this.log('debug', 'websocket closed')
+	// 		}
+	// 	} catch (e) {
+	// 		this.log('error', `Cannot connect to websocket: ${e}`)
+	// 	}
+	// }
+
+	private createOSCServer() {
+		try {
+			this.oscServer = new Server(this.config.rec_port, '0.0.0.0')
+			this.oscServer.on('error', (err) => {
+				this.log('error', `Error in OSC Server: ${err.message}`)
+			})
+			this.oscServer.on('message', (msg) => {
+				this.log('debug', msg[0].toString())
+			})
+			this.oscServer.on('listening', () => {
+				this.log('debug', `OSC Server listening on ${this.config.rec_port}`)
+				this.updateStatus(InstanceStatus.Ok)
+			})
+		} catch {
+			this.log('error', `Cannot create server on port ${this.config.rec_port}`)
+		}
+	}
+
+	private createClient() {
+		this.consoleClient = new Client(this.config.target, this.config.send_port)
+	}
+
+	private connectToConsole() {
+		this.consoleClient?.send('/Console/Name/?')
+		for (let i = 1; i <= 36; i++) {
+			this.consoleClient?.send(`/Control_Groups/${i}/fader/?`)
+		}
 	}
 }
 
